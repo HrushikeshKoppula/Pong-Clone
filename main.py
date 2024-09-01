0fcc86a1aee5280c9e89e24b0f1cfc487a72468a
HrushikeshKoppula <95178027+HrushikeshKoppula@users.noreply.github.com>
2024-09-01 07:01:36 +0530

    Done!

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ class PLAYER:
         self.height = height
         self.color = color
         self.speed = speed
+        self.points = 0
         self.rlim = screen_width-width
         self.ulim = 0
         self.llim = 0
@@ -43,13 +44,61 @@ class PLAYER:
         if self.top>self.dlim:
             self.top=self.dlim
 
+class BALL:
+    def __init__(self):
+        self.velx = 2
+        self.vely = 4
+        self.centrex = screen_width//2
+        self.centrey = screen_height//2
+
+    def collision_control(self):
+        self.rect = pygame.Rect(self.centrex-5, self.centrey-5,10,10)
+        Player1.rect = pygame.Rect(Player1.left, Player1.top, Player1.width, Player1.height)
+        Player2.rect = pygame.Rect(Player2.left, Player2.top, Player2.width, Player2.height)
+        if self.rect.colliderect(Player1.rect) or self.rect.colliderect(Player2.rect):
+            self.velx = -self.velx
+
+    def Move(self):
+        self.centrex += self.velx
+        self.centrey += self.vely
+        if self.centrey < 5 or self.centrey > screen_height - 5:
+            self.vely = -self.vely
+        if self.centrex < 5:
+            Player2.points += 1
+            self.centrex = screen_width//2
+            self.centrey = screen_height//2
+        if self.centrex > screen_width - 5:
+            Player1.points += 1
+            self.centrex = screen_width//2
+            self.centrey = screen_height//2
+
+    def Draw(self):
+        pygame.draw.circle(screen, "white", (self.centrex, self.centrey), 5)
+
+def BoardDraw(Player1, Player2, Ball):
+    Ball.collision_control()
+    Ball.Move()
+    Player1.Draw()
+    Player2.Draw()
+    Ball.Draw()
+    p1 = font.render(str(Player1.points),True,(100, 100, 100))
+    p1_rect = p1.get_rect(center=(screen_width//4, screen_height//4))
+    screen.blit(p1, p1_rect)
+    p2 = font.render(str(Player2.points),True,(100, 100, 100))
+    p2_rect = p2.get_rect(center=(3*screen_width//4, screen_height//4))
+    screen.blit(p2, p2_rect)
+    for i in range(45):
+        pygame.draw.rect(screen, (100, 100, 100),(screen_width//2-2, i*16, 4, 8))
+
+
 playing = False
 controls_info = font.render("Controls:   Player - 1: W, S    Player - 2: UP, Down",True,"white")
 controls_rect = controls_info.get_rect(center=(screen_width//2,screen_height//2-200))
 start_info = font.render("Press SPACE to start the game",True,"white")
 start_rect = start_info.get_rect(center=(screen_width//2,screen_height//2+200))
-Player1 = PLAYER(0, screen_height//2, 10, 40, "white")
-Player2 = PLAYER(screen_width - 10, screen_height//2, 10, 40, "white")
+Player1 = PLAYER(0, screen_height//2-20, 10, 40, "white")
+Player2 = PLAYER(screen_width - 10, screen_height//2-20, 10, 40, "white")
+Ball = BALL()
 
 while running:
     for event in pygame.event.get():
@@ -70,9 +119,7 @@ while running:
         if keys[pygame.K_DOWN]:
             Player2.Down()
 
-        Player1.Draw()
-        Player2.Draw()
-
+        BoardDraw(Player1, Player2, Ball)
         
 
     elif keys[pygame.K_SPACE]:
